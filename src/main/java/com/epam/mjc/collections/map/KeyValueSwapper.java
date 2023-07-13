b1c49825d53366e2158d16fe69743f1c4243a510
alisher-erbolat <alisher.001017@gmail.com>
2023-07-13 16:18:41 +0600

    done

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,22 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        Map<String, Integer> map = new HashMap<>();
+
+        for (Map.Entry<Integer, String> e : sourceMap.entrySet()){
+            if (!map.containsKey(e.getValue())){
+                map.put(e.getValue(), e.getKey());
+            }
+            else{
+                if (map.get(e.getValue())>e.getKey()){
+                    map.put(e.getValue(), e.getKey());
+                }
+            }
+        }
+        return map;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,29 @@
 package com.epam.mjc.collections.map;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        if (sentence.isEmpty()) return new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
+        sentence = sentence.replace(",","");
+        sentence = sentence.replace(".","");
+
+        ArrayList<String> words = new ArrayList<>(Arrays.asList(sentence.toLowerCase().split(" ")));
+        for (int i = 0; i < words.size(); i++) {
+            if(!map.containsKey(words.get(i))){
+                int repeat = 1;
+                for (int j = i +1; j < words.size(); j++) {
+                    if (words.get(i).equalsIgnoreCase(words.get(j))){
+                        repeat++;
+                    }
+                }
+                map.put(words.get(i), repeat);
+            }
+        }
+        return map;
     }
 }
